2a2b5cf51e8951fc7f8de0f9bf3ae21e412f9580
Ducan2003 <nguyenducan1172003@gmail.com>
2023-06-08 21:49:34 +0700

    project1

---
+++ Main.java
@@ -0,0 +1,16 @@
+package Bai3;
+
+
+public class Main {
+	public static  void main(String[] args) {
+		Triangle st1 = new Triangle (4, 6, 3);
+		Triangle st2 = new Triangle (8,10,5);
+		Triangle st3 = new Triangle (5,1,5);
+		System.out.println(st1.toString());
+		System.out.println(st2.toString());
+		System.out.println(st3.toString());
+	}
+	}
+	
+		
+		
